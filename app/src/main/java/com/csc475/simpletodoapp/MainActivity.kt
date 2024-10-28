279f3bfb0ad91e93aaba851954982255152f99bd
bdurian10 <braden.durian@csuglobal.edu>
2024-10-27 19:18:34 -0600

    Second Commit: implemented delete button

--- app/src/main/java/com/csc475/simpletodoapp/MainActivity.kt
+++ app/src/main/java/com/csc475/simpletodoapp/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var dbHelper: DatabaseHelper
     private lateinit var adapter: TodoAdapter
-    private lateinit var todoList: MutableList<TodoItem>
+    private lateinit var todoList: MutableList<TodoItem>  // Use MutableList here
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,13 +19,22 @@ class MainActivity : AppCompatActivity() {
 
         // Initialize DatabaseHelper and load tasks from database
         dbHelper = DatabaseHelper(this)
-        todoList = dbHelper.getAllTodoItems().toMutableList()
+        todoList = dbHelper.getAllTodoItems().toMutableList()  // Convert to MutableList
 
-        // Set up RecyclerView and Adapter
+        // Set up RecyclerView and Adapter with update and delete handlers
         val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
-        adapter = TodoAdapter(todoList) { item ->
-            dbHelper.updateTodoItem(item)  // Update item in database when completed status changes
-        }
+        adapter = TodoAdapter(todoList,
+            onItemUpdated = { item ->
+                dbHelper.updateTodoItem(item)  // Update item in database when completed status changes
+            },
+            onItemDeleted = { item ->
+                // Delete item from database and list
+                dbHelper.deleteTodoItem(item.id)
+                val position = todoList.indexOf(item)
+                todoList.removeAt(position)  // This will now work as todoList is Mutable
+                adapter.notifyItemRemoved(position)
+            }
+        )
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = adapter
 
@@ -44,3 +53,5 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+
+
--- app/src/main/java/com/csc475/simpletodoapp/TodoAdapter.kt
+++ app/src/main/java/com/csc475/simpletodoapp/TodoAdapter.kt
@@ -3,18 +3,21 @@ package com.csc475.simpletodoapp
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.CheckBox
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class TodoAdapter(
-    private val todoList: List<TodoItem>,
-    private val onItemUpdated: (TodoItem) -> Unit
+    private val todoList: MutableList<TodoItem>,
+    private val onItemUpdated: (TodoItem) -> Unit,
+    private val onItemDeleted: (TodoItem) -> Unit
 ) : RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
 
     inner class TodoViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val title: TextView = view.findViewById(R.id.todoTitle)
         val checkBox: CheckBox = view.findViewById(R.id.todoCheckBox)
+        val deleteButton: Button = view.findViewById(R.id.deleteButton)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
@@ -27,11 +30,25 @@ class TodoAdapter(
         val item = todoList[position]
         holder.title.text = item.title
         holder.checkBox.isChecked = item.isCompleted
+        //Toggle checkbox when pressed
         holder.checkBox.setOnCheckedChangeListener { _, isChecked ->
             item.isCompleted = isChecked
             onItemUpdated(item)
         }
+        //Delete item when delete button is pressed
+        holder.deleteButton.setOnClickListener{
+            onItemDeleted(item)
+        }
     }
 
     override fun getItemCount() = todoList.size
+
+    /*Method to remove an item from the list
+    fun removeItem(position: Int){
+        todoList.removeAt(position)
+        notifyItemRemoved(position)
+    }
+
+     */
+
 }
